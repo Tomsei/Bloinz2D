bc931f410598d308e075959fdd22fdc85e37783d
Sebastian <sebastian.rindfleisch@study.hs-duesseldorf.de>
2019-12-05 14:58:07 +0100

    Szenenwechsel vom malen zurueck optimiert

--- Skripte/Malen.gd
+++ Skripte/Malen.gd
@@ -351,8 +351,8 @@ func _on_Button2_button_up():
 
 
 func _on_Zurueck_button_up():
-	get_tree().change_scene("res://Szenen/Player.tscn")
-	OS.set_window_size(Vector2(384,512))
+	get_tree().change_scene("res://Szenen/Spieloberflaeche.tscn")
+	OS.set_window_size(Vector2(448,640))
 
 
 func _on_Fuellen_pressed():
